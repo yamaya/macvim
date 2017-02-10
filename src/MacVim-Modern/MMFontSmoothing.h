21bb829a341f4844e939e2bd3c4fadd6a0e9f494
Masayuki Yamaya <yamaya@cyberdom.co.jp>
2017-02-10 22:19:39 +0900

    Refactor

---
+++ src/MacVim-Modern/MMFontSmoothing.h
@@ -0,0 +1,12 @@
+#import <Cocoa/Cocoa.h>
+
+@interface MMFontSmoothing : NSObject
+
+@property (nonatomic, readonly) CGContextRef context;
+@property (nonatomic, readonly) BOOL enabled;
+
+- (instancetype)initWithContext:(CGContextRef)context enabled:(BOOL)enabled;
+
++ (instancetype)fontSmoothingEnabled:(BOOL)enabled on:(CGContextRef)context;
+
+@end
---
+++ src/MacVim-Modern/MMFontSmoothing.m
@@ -0,0 +1,41 @@
+#import "MMFontSmoothing.h"
+#import "MMCoreTextView+Compatibility.h"
+
+@implementation MMFontSmoothing
+{
+  int _style;
+}
+@synthesize context = _context, enabled = _enabled;
+
++ (instancetype)fontSmoothingEnabled:(BOOL)enabled on:(CGContextRef)context
+{
+    return [[MMFontSmoothing alloc] initWithContext:context enabled:enabled];
+}
+
+- (instancetype)initWithContext:(CGContextRef)context enabled:(BOOL)enabled
+{
+    if ((self = [super init]) != nil) {
+        _context = context;
+        _enabled = enabled;
+        if (_enabled) [self setStyle:fontSmoothingStyleLight];
+    }
+    return self;
+}
+
+- (void)dealloc
+{
+    if (_enabled) [self unsetStyle];
+}
+
+- (void)setStyle:(int)style
+{
+    _style = CGContextGetFontSmoothingStyle(_context);
+    CGContextSetFontSmoothingStyle(_context, style);
+}
+
+- (void)unsetStyle
+{
+    CGContextSetFontSmoothingStyle(_context, _style);
+}
+
+@end
