7478fd0aa0c76fbe88066aa58a30799e32f0c5fe
Masayuki Yamaya <yamaya@cyberdom.co.jp>
2017-02-14 00:15:21 +0900

    MMApplication arc-nized.

--- src/MacVim-Modern/MMApplication.m
+++ src/MacVim-Modern/MMApplication.m
@@ -20,46 +20,36 @@
 
 - (void)sendEvent:(NSEvent *)event
 {
-    NSEventType type = [event type];
-    unsigned flags = [event modifierFlags];
+    const NSEventType type = event.type;
+    unsigned flags = event.modifierFlags;
 
     // HACK! Intercept 'help' key presses and clear the 'help key flag', else
     // Cocoa turns the mouse cursor into a question mark and goes into 'context
     // help mode' (the keyDown: event itself never reaches the text view).  By
     // clearing the 'help key flag' this event will be treated like a normal
     // key event.
-    if ((NSEventTypeKeyDown == type || NSEventTypeKeyUp == type) &&
-            (flags & NSEventModifierFlagHelp)) {
+    if ((NSEventTypeKeyDown == type || NSEventTypeKeyUp == type) && (flags & NSEventModifierFlagHelp)) {
         flags &= ~NSEventModifierFlagHelp;
-        event = [NSEvent keyEventWithType:[event type]
-                                 location:[event locationInWindow]
+        event = [NSEvent keyEventWithType:type
+                                 location:event.locationInWindow
                             modifierFlags:flags
-                                timestamp:[event timestamp]
-                             windowNumber:[event windowNumber]
+                                timestamp:event.timestamp
+                             windowNumber:event.windowNumber
                                   context:nil // [event context] is always nil
-                               characters:[event characters]
-              charactersIgnoringModifiers:[event charactersIgnoringModifiers]
-                                isARepeat:[event isARepeat]
-                                  keyCode:[event keyCode]];
+                               characters:event.characters
+              charactersIgnoringModifiers:event.charactersIgnoringModifiers
+                                isARepeat:event.isARepeat
+                                  keyCode:event.keyCode];
     }
-
     [super sendEvent:event];
 }
 
 - (void)orderFrontStandardAboutPanel:(id)sender
 {
-    NSString *version = [[NSBundle mainBundle] objectForInfoDictionaryKey:
-            @"CFBundleVersion"];
-    NSString *marketingVersion = [[NSBundle mainBundle]
-            objectForInfoDictionaryKey:@"CFBundleShortVersionString"];
-    NSString *title = [NSString stringWithFormat:
-            @"Custom Version %@ (%@)", marketingVersion, version];
-
-    [self orderFrontStandardAboutPanelWithOptions:
-            [NSDictionary dictionaryWithObjectsAndKeys:
-                @"",    @"Version",
-                title,  @"ApplicationVersion",
-                nil]];
+    NSString *version = [NSBundle.mainBundle objectForInfoDictionaryKey:@"CFBundleVersion"];
+    NSString *marketingVersion = [NSBundle.mainBundle objectForInfoDictionaryKey:@"CFBundleShortVersionString"];
+    NSString *title = [NSString stringWithFormat:@"Custom Version %@ (%@)", marketingVersion, version];
+    [self orderFrontStandardAboutPanelWithOptions:@{@"Version": @"", @"ApplicationVersion": title}];
 }
 
 @end
