a2abf1b7326d650d2a829c1028327a5a2f65dceb
Masayuki Yamaya <yamaya@cyberdom.co.jp>
2017-02-15 22:22:22 +0900

    Remove MMCoreTextRender

--- src/MacVim-Modern/MMCoreTextRenderer.h
+++
@@ -1,4 +0,0 @@
-#import <Cocoa/Cocoa.h>
-#import <CoreText/CoreText.h>
-
-extern void RecurseDraw(const unichar *chars, CGGlyph *glyphs, CGPoint *positions, UniCharCount length, CGContextRef context, CTFontRef fontRef, NSMutableArray *fontCache, BOOL useLigatures);
